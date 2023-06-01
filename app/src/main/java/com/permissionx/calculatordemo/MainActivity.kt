8b6ade4c474713a0f86a0bdc5f28817fa24989cb
Mayberry <2673220823@qq.com>
2023-06-01 23:24:38 +0800

    添加数字按钮和清空按钮点击事件，实现将表达式显示在屏幕上功能

--- app/src/main/java/com/permissionx/calculatordemo/MainActivity.kt
+++ app/src/main/java/com/permissionx/calculatordemo/MainActivity.kt
@@ -2,10 +2,64 @@ package com.permissionx.calculatordemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var isNumStart = true
+    private val currentInputStringBuilder = StringBuilder()
+    private val numsList = mutableListOf<Int>()
+    private val operatorList = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun numberButtonClicked(view: View) {
+        val textView = view as TextView
+        currentInputStringBuilder.append(textView.text)
+        if (isNumStart) {
+            numsList.add(textView.text.toString().toInt())
+            isNumStart = false
+        } else {
+            numsList[numsList.size - 1] = currentInputStringBuilder.toString().toInt()
+        }
+        showUI()
+
+    }
+
+    fun operatorButtonClicked(view: View) {
+        val textView = view as TextView
+        operatorList.add(textView.text.toString())
+        currentInputStringBuilder.clear()
+        isNumStart = true
+        showUI()
+    }
+
+    fun clearButtonClicked(view: View) {
+        currentInputStringBuilder.clear()
+        process_textView.text = ""
+    }
+
+    fun backButtonClicked(view: View) {
+        Log.v("MainActivity", "back")
+    }
+
+    fun equalButtonClicked(view: View) {
+        Log.v("MainActivity", "equal")
+    }
+
+    private fun showUI() {
+        val sb = StringBuilder()
+        for ((i, num) in numsList.withIndex()) {
+            sb.append(num)
+            if (i < operatorList.size) {
+                sb.append(" ${operatorList[i]} ")
+            }
+        }
+        process_textView.text = sb.toString()
     }
 }
